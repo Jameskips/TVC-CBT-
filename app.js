9ce0ae451d7ca34458d978654fe975cee06d86dc
Jameskips <jameskips024@gmail.com>
2025-08-30 03:58:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // --- JSONBin.io Config ---
 const BIN_ID = "68b25b02d0ea881f406ad0d3"; 
 const API_KEY = "$2a$10$woiCOWuiTMmOSJbk8sjbuuxwKHeloo1x1uytuEkzE16Bcc9YP898y";
-const BIN_URL = `https://api.jsonbin.io/v3/b/${68b25b02d0ea881f406ad0d3}`;
+const BIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 
 // Data placeholders
 let students = {};
